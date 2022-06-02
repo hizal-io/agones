a6ef6621735620e27366590a8858bec6fac6043f
Anthony <cvballa3g0@gmail.com>
2022-06-01 23:00:16 -0400

    k8s: update resource proto file

--- build/gen-crd-protos.sh
+++ build/gen-crd-protos.sh
@@ -4,14 +4,15 @@ echo "Generating protos"
 # To support running this script from anywhere, first cd into this directory,
 # and then install with forced module mode on and fully qualified name.
 cd "$(dirname "${0}")"
+cd ..
 
 go install golang.org/x/tools/cmd/goimports@v0.1.7
 go install k8s.io/code-generator/cmd/go-to-protobuf@v0.24.1
 
 $GOPATH/bin/go-to-protobuf \
-  		--go-header-file=boilerplate.go.txt \
+  		--go-header-file=build/boilerplate.go.txt \
   		--packages=agones.dev/agones/pkg/apis/agones/v1 \
-  		--apimachinery-packages=-k8s.io/apimachinery/pkg/util/intstr,-k8s.io/apimachinery/pkg/api/resource,-k8s.io/apimachinery/pkg/apis/meta/v1,-k8s.io/api/core/v1,-k8s.io/api/apps/v1,-k8s.io/apimachinery/pkg/runtime/schema \
+  		--apimachinery-packages=-k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,-k8s.io/apimachinery/pkg/apis/meta/v1,-k8s.io/api/core/v1,-k8s.io/api/apps/v1,-k8s.io/apimachinery/pkg/runtime/schema \
   		--proto-import=${GOPATH}/src/agones.dev/agones/vendor \
   		--proto-import=${GOPATH}/pkg/mod
 
