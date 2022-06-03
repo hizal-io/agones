f14a8ab05df43344f2eb02040b284a07003cb6de
Anthony <cvballa3g0@gmail.com>
2022-06-02 23:30:02 -0400

    gen autoscaling protos

--- build/gen-crd-protos.sh
+++ build/gen-crd-protos.sh
@@ -12,16 +12,17 @@ go install k8s.io/code-generator/cmd/go-to-protobuf@v0.24.1
 $GOPATH/bin/go-to-protobuf \
   		--go-header-file=build/boilerplate.go.txt \
   		--packages=agones.dev/agones/pkg/apis/agones/v1 \
-  		--apimachinery-packages=-k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,-k8s.io/apimachinery/pkg/apis/meta/v1,-k8s.io/api/core/v1,-k8s.io/api/apps/v1,-k8s.io/apimachinery/pkg/runtime/schema \
+  		--apimachinery-packages=-k8s.io/apimachinery/pkg/util/intstr,-k8s.io/apimachinery/pkg/api/resource,-k8s.io/apimachinery/pkg/apis/meta/v1,-k8s.io/api/core/v1,-k8s.io/api/apps/v1,-k8s.io/apimachinery/pkg/runtime/schema \
   		--proto-import=${GOPATH}/src/agones.dev/agones/vendor \
   		--proto-import=${GOPATH}/pkg/mod
 
 $GOPATH/bin/go-to-protobuf \
   		--go-header-file=build/boilerplate.go.txt \
-  		--packages=agones.dev/agones/pkg/apis/autoscaling/v1 \
-  		--apimachinery-packages=-k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,-k8s.io/apimachinery/pkg/apis/meta/v1,-k8s.io/api/core/v1,-k8s.io/api/apps/v1,-k8s.io/apimachinery/pkg/runtime/schema \
+  		--packages=agones.dev/agones/pkg/apis/autoscaling/v1,-agones.dev/agones/pkg/apis/agones/v1=agones.v1 \
+  		--apimachinery-packages=-k8s.io/apimachinery/pkg/util/intstr,-k8s.io/apimachinery/pkg/api/resource,-k8s.io/apimachinery/pkg/apis/meta/v1,-k8s.io/api/core/v1,-k8s.io/api/apps/v1,-k8s.io/apimachinery/pkg/runtime/schema,-k8s.io/api/admissionregistration/v1,-k8s.io/apimachinery/pkg/runtime \
   		--proto-import=${GOPATH}/src/agones.dev/agones/vendor \
   		--proto-import=${GOPATH}/pkg/mod
+
 
 # go install k8s.io/code-generator/cmd/go-to-protobuf@v0.21.5
 # git clone git@github.com:hizal-io/agones.git $GOPATH/src/agones.dev/agones
